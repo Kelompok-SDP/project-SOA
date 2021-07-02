eee8a0488ad1e1f689a559265eaf8844afec028c
micshanwid7 <54869991+micshanwid7@users.noreply.github.com>
2021-07-02 17:52:37 +0700

    Update Users.js

--- Routes/Users.js
+++ Routes/Users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const uploadFile = require("../uploadFile");
+const uploadFile = require("../upload");
 const User = require("../Models/Users");
 const Produk = require("../Models/Products");
 const auth = require('../autentikasi');
@@ -16,9 +16,76 @@ async function getUser(req,res){
     return user;
 }
 
-router.post('/register',uploadFile.upload.single("foto_user"),async (req, res) => {
+const fs = require("fs");
+const multer = require("multer");
+const OAuth2Data = require("./credentials.json");
+var name,pic
+
+const { google } = require("googleapis");
+
+const CLIENT_ID = OAuth2Data.client_id;
+const CLIENT_SECRET = OAuth2Data.client_secret;
+const REDIRECT_URL = OAuth2Data.redirect_uris[0];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL
+);
+var authed = false;
+
+// If modifying these scopes, delete token.json.
+const SCOPES =
+  "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/userinfo.profile";
+
+var Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./public/uploads");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  },
+});
+
+var upload = multer({
+  storage: Storage,
+}).single("foto_user"); //Field name and max count
+
+router.post('/register',async (req, res) => {
+    upload(req, res, function (err) {
+        if (err) {
+          console.log(err);
+          return res.end("Something went wrong");
+        } else {
+          console.log(req.file.path);
+          const drive = google.drive({ version: "v3",auth:oAuth2Client  });
+          const fileMetadata = {
+            name: req.file.filename,
+          };
+          const media = {
+            mimeType: req.file.mimetype,
+            body: fs.createReadStream(req.file.path),
+          };
+          drive.files.create(
+            {
+              resource: fileMetadata,
+              media: media,
+              fields: "id",
+            },
+            (err, file) => {
+              if (err) {
+                // Handle error
+                console.error(err);
+              } else {
+                fs.unlinkSync(req.file.path)
+              }
+            }
+          );
+        }
+      });
+
     let {nama, email,password,telepon,jenis_kelamin} = req.body;
-    let foto_user = "./Public/uploads/"+req.file.filename;
+    let foto_user = "./public/uploads/"+req.file.filename;
 
     nama = nama.toString().charAt(0).toUpperCase()+nama.toString().slice(1);
     let tipe_user = 0;
